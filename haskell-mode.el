de3081257867339bc2273d2454c6fb94c68fe550
Michael Sloan <mgsloan@gmail.com>
2018-12-01 05:17:55 -0800

    Bump version over 9000

--- haskell-mode.el
+++ haskell-mode.el
@@ -12,7 +12,7 @@
 ;;          2003      Dave Love <fx@gnu.org>
 ;;          2016      Arthur Fayzrakhmanov
 ;; Keywords: faces files Haskell
-;; Version: 16.2-git
+;; Version: 9001-git
 ;; URL: https://github.com/haskell/haskell-mode
 
 ;; This file is not part of GNU Emacs.
@@ -46,7 +46,7 @@
 ;; All functions/variables start with `(literate-)haskell-'.
 
 ;; Version of mode.
-(defconst haskell-version "16.2-git"
+(defconst haskell-version "9001-git"
   "The release version of `haskell-mode'.")
 
 ;;;###autoload
